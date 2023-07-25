d6d49ab3ca903a1ecce10d66d4923ab64de4409f
Mike Gvozdev <mv.gvozdev@gmail.com>
2023-07-25 10:54:34 -0700

    Change page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AI Getting Started",
+  title: "AI Friend",
   description: "Help you setup an AI project with ease",
 };
 
